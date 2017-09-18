ff0c360c59d7369fcb08bf64b1b3ece1779c1bf7
geladd <geladd@126.com>
2017-09-18 21:29:14 +0800

    添加管理端界面

---
+++ application/modules/Admin/controllers/Index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @name IndexController
+ * @author root
+ * @desc 后台默认控制器
+ * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
+ */
+class IndexController extends Yaf_Controller_Abstract {
+
+	/** 
+     * 默认动作
+     */
+	public function indexAction($name = "Stranger") {
+		/*$config = Yaf_Application::app()->getConfig()->db;
+        $db = Db_MySql::getInstance($config);
+        $rows = $db->fetchAll('select * from `shop`');
+        $this->getView()->assign("rows", $rows);*/
+        return TRUE;
+	}
+}
