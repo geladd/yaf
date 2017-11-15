5c8fb6a6dbd4ce77a54d10efadefb535e7d46ef3
ywhou <ywhou@3ti.us>
2017-11-15 09:35:24 +0800

    修改

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -30,9 +30,9 @@ class IndexController extends Yaf_Controller_Abstract {
         echo '<pre>';
         //print_r($config);
         //print_r($config1);
-        echo $config['redis']['cache']['host'].'<br>';
+        /*echo $config['redis']['cache']['host'].'<br>';
         echo $config1['redis']['cache']['port'].'<br>';
-        echo $config->db->type;
+        echo $config->db->type;*/
         echo '</pre>';
 
 		//4. render by Yaf, 如果这里返回FALSE, Yaf将不会调用自动视图引擎Render模板
